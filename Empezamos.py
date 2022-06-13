335aa157b4b1a9bc7ba1f0aad2633e0c6316b883
Jose Pablo <sortzweb@gmail.com>
2022-06-13 12:02:47 +0200

    master

--- Empezamos.py
+++ Empezamos.py
@@ -1,4 +1,20 @@
 print("Hello")
 a=3
-print(a)
 
+#Ya sabemos que floats no se pueden comparar
+#No obstante podemos compararlos con la mayor 
+#precision posible de nuestro sistema
+
+import sys
+import math
+
+def compararFloat(a,b):
+    if abs(a-b) < sys.float_info.epsilon:
+        print("Son iguales")
+    else:
+        print("Son distintos")
+
+
+b=3.0000000000000000000000000000000000000001
+
+compararFloat(a,b)
